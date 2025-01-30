da71aa908c747896fd21c49a574fab9dc009d30a
Subhajit Das <dassubhajit@gmail.com>
2025-01-30 19:00:15 +0530

    feat:add generic renderer and profile celery tasks

---
+++ core_apps/profiles/tasks.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+from celery import shared_task
+from .models import Profile
+import cloudinary.uploader
+
+@shared_task(name="upload_avatar_to_clodinary")
+def upload_avatar_to_clodinary(profile_id:UUID,image_content:bytes)->None:
+    profile=Profile.objects.get(id=profile_id)
+    response=cloudinary.uploader.upload(image_content)
+    profile.avatar=response["url"]
+    profile.save()
+
+@shared_task(name="update_all_reputations")
+def update_all_reputations():
+    for profile in Profile.objects.all():
+        profile.update_reputation()
+        profile.save()
