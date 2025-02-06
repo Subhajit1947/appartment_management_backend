b209db7eaa97c10c111115642074b850090eb6eb
Subhajit Das <dassubhajit@gmail.com>
2025-02-06 10:20:52 +0530

    feat:user can report other user for missconduct

---
+++ core_apps/reports/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics,serializers
+from .models import Report
+from .serializers import ReportSerializers
+from core_apps.common.renderers import GenericJSONRenderer
+
+class ReportCreateAPIView(generics.CreateAPIView):
+    queryset=Report.objects.all()
+    serializer_class=ReportSerializers
+    renderer_classes=[GenericJSONRenderer]
+    object_label="report"
+
+    def perform_create(self, serializer:serializers.Serializer)->None:
+        serializer.save(reported_by=self.request.user)
+
+class ReportListAPIView(generics.ListAPIView):
+   
+    serializer_class=ReportSerializers
+    renderer_classes=[GenericJSONRenderer]
+    object_label="reports"
+
+    def get_queryset(self)->Report:
+        user=self.request.user
+        return Report.objects.filter(reported_by=user)
+
+
+
+
+
+
+
+
+
