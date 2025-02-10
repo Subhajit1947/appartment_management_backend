dc02ca8f2e66b9fa1316d00649ead54186a509b5
Subhajit Das <dassubhajit@gmail.com>
2025-02-10 11:14:49 +0530

    chor: setup custom font eslint prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Home page abcd djj subhajit !!!</h1>
+      <h1 className="font-robotoSlab text-6xl">Home page abcd djj subhajit !!!</h1>
+      <p className="font-openSans text-3xl">
+        there was this one dog that was too gready
+      </p>
     </div>
   );
 }
