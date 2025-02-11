50a1faeb31e7e580f38ba19c8fc063bad43d3a97
Subhajit Das <dassubhajit@gmail.com>
2025-02-11 14:53:05 +0530

    feat:add theme switcher component

---
+++ client/src/app/(estate)/layout.tsx
@@ -0,0 +1,32 @@
+import Navbar from '@/components/shared/navbar/Navbar'
+import React from 'react'
+
+interface LayoutProps{
+    children:React.ReactNode
+}
+
+
+export default function layout({children}:LayoutProps) {
+  return (
+    <main className='bg-baby_veryBlack relative'>
+        {/* <h2 className='dark:text-pumpkin text-3xl'>Navbar</h2> */}
+        <Navbar/>
+        <div className='flex'>
+            {/* placeholder LeftNavbar component */}
+            <div className='dark:text-pumpkin hidden text-xl md:block'>
+                Left Navbar
+
+            </div>
+            <section className='flex min-h-screen flex-1 flex-col px-4 pb-6 pt-24 sm:px-6 lg:px-8 lg:pt-32'>
+                <div>{children}</div>
+            </section>
+            {/* placeholder RightNavbar component */}
+            <div className='dark:text-pumpkin hidden text-xl md:block'>
+                Right Navbar
+
+            </div>
+            
+        </div>
+    </main>
+  )
+}
