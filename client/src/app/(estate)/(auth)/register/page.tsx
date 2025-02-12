8515a5167a9c00c9ee44abd9394ed4da276118f0
Subhajit Das <dassubhajit@gmail.com>
2025-02-12 18:30:52 +0530

    feat:add auth form header

--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,9 +1,16 @@
+import { AuthFormHeader } from '@/components/forms/auth'
 import React from 'react'
 
 export default function RegisterPage() {
   return (
     <div>
-        <h1 className='dark:text-pumpkin text-6xl'>Register Page</h1>
+        {/* <h1 className='dark:text-pumpkin text-6xl'>Register Page</h1> */}
+        <AuthFormHeader 
+          title='signup for an account' 
+          staticText='Already have an account?'
+          linkText='Login Here'
+          linkHref='/login'
+        />
     </div>
   )
 }
