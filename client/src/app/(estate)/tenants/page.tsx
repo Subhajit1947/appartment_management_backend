0bef39dfa914b186481914a36232ad094ae150c5
Subhajit Das <dassubhajit@gmail.com>
2025-02-21 20:30:32 +0530

    feat:add teant pagination component

--- client/src/app/(estate)/tenants/page.tsx
+++ client/src/app/(estate)/tenants/page.tsx
@@ -1,41 +1,28 @@
-"use client"
-import ProtectedRoute from '@/components/shared/ProtectedRoutes'
-import Spinner from '@/components/shared/Spinner'
-import { useGetAllUsersQuery } from '@/lib/redux/features/users/usersApiSlice'
-import React from 'react'
 
-function TenantsPageContent() {
-    const {data,isLoading}=useGetAllUsersQuery({})
-    if(isLoading){
-        return(
-            <div className='flex-center pt-32'>
-                <Spinner size="xl"/>
-            </div>
-        )
-    }
-  return (
-    <div>
-        <h1 className='dark:text-pumpkin text-6xl'>
-            Tenants
-        </h1>
-        {data && data.profiles.results.length>0?(
-            data.profiles.results.map((tenant)=>(
-                <p key={tenant.id}
-                    className='text-2xl dark:text-lime-500'
-                >{tenant.full_name}-{tenant.occupation}</p>
-            ))
-        ):(
-           <p>No Tenants Found</p> 
-        )}
-    </div>
-  )
+
+import TenantCard from "@/components/cards/TenantCard"
+import ProtectedRoute from "@/components/shared/ProtectedRoutes"
+import type { Metadata } from "next"
+
+export const metadata:Metadata={
+    title:"Alpha Apartments | Tenants",
+    description:"Authenticated users can view basic information abount othertenants with in the property. Tenants can also search for other tenants"
 }
+
+function TenantsPageContent(){
+    return(
+        <div>
+            <TenantCard/>
+        </div>
+    )
+}
+
 
 export default function TenantsPage(){
     return(
-        <ProtectedRoute>
+       <ProtectedRoute>
             <TenantsPageContent/>
-        </ProtectedRoute>
+       </ProtectedRoute>
     )
 }
 
---
+++ client/src/lib/redux/features/users/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+
+
+const initialState={
+    searchTerm:"",
+    page:1
+}
+
+const userSlice=createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        setSearchTerm:(state,action:PayloadAction<string>)=>{
+            state.searchTerm=action.payload
+        },
+        setCurrentPage:(state,action:PayloadAction<number>)=>{
+            state.page=action.payload
+        },
+    }
+})
+
+export const {setSearchTerm,setCurrentPage}=userSlice.actions
+export default userSlice.reducer
+
+
+
+
